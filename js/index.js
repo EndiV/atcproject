5019485280785cf8c2c34abf7a92985abf3303a3
Endi V <64462777+EndiV@users.noreply.github.com>
2020-09-20 15:53:01 +0200

    update

--- js/index.js
+++ js/index.js
@@ -8,16 +8,42 @@
 // document.write(registry.student + " you are from " + registry.country + " and you are " + registry.age + " years old" );
 
 
-var registry ={
-    "student": "Endi Vulka",
-    "country" : "Albania",
-    "age": "27",
-    informacion: function (){
-        return this.student + " you are from " + this.country 
-    }
-}
-document.write(registry.informacion());
-// document.write(registry.student + " you are from " + registry.country + " and you are " + registry.age + " years old" );
-document.getElementsByTagName("h1")[0].addEventListener("click",function(){
-    this.innerHTML="CKEMI"
-})
+// var registry ={
+//     "student": "Endi Vulka",
+//     "country" : "Albania",
+//     "age": "27",
+//     informacion: function (){
+//         return this.student + " you are from " + this.country 
+//     }
+// }
+// document.write(registry.informacion());
+// // document.write(registry.student + " you are from " + registry.country + " and you are " + registry.age + " years old" );
+// document.getElementsByTagName("h1")[0].addEventListener("click",function(){
+//     this.innerHTML="CKEMI"
+// })
+
+
+// array qe permeban dy element edhe permban property 
+
+// var books = [{
+//     title: "Kumbulla pertej murit",
+//     author: "Ernest koliqi",
+//     alreadyRead : "true",
+//     img:"https://upload.wikimedia.org/wikipedia/en/c/c0/Harry_Potter_and_the_Chamber_of_Secrets_movie.jpg"
+
+// },{
+//     title: "Harry potter and the chamber of secrets",
+//     author: "J.K. Rowling",
+//     alreadyRead : "False",
+//     img:"https://upload.wikimedia.org/wikipedia/en/c/c0/Harry_Potter_and_the_Chamber_of_Secrets_movie.jpg"
+
+// }
+// ];
+
+// for (i=0; i<books.length;i++){
+//     var p = document.createElement('p');
+//     var node  = document.createTextNode(books[i].title + ' by ' + books[i].author);
+//     p.appendChild(node);
+//     document.getElementById("one").appendChild(p);
+// }document.body.style.fontFamily = "Arial, sans-serif";
+document.getElementById("name").textContent= "Endi";
